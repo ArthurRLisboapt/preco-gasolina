615f5126aa2e49160b004af41114ca6bca5f1397
arthurRLisboapt <arthurrlisboa@gmail.com>
2024-01-25 09:46:46 +0000

    Python e gráfico de linha de vendas de gasolina

---
+++ gasolina.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+precogasolina_csv = 'gasolina.csv'
+dados = pd.read_csv(precogasolina_csv)
+dados
+
+plt.figure(figsize=(10, 6))
+plt.plot(dados['dia'], dados['venda'], marker='o', linestyle='-', color='b')
+plt.title('Vendas de Gasolina por Dia')
+plt.xlabel('Dia')
+plt.ylabel('Preço de Venda')
+plt.savefig('gasolina.png')
